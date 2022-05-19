c6c69cc9f847b41a6ac6bf1a57e762de522ecaf0
Vinicius Mendes <103964322+ViniciusMendez@users.noreply.github.com>
2022-05-19 19:25:49 -0300

    novos exercicio

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var  vel = 30.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60 ) { // condição simples!
+    console.log('voce ultrapasspu a velociade limite. MULTADO!')
+}
+console.log('Dirija com cuidado e USA A PORRA DO CINTO')

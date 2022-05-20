dc0c3665e4aa342ba9aa3977bcf36da48d42af8b
Vinicius Mendes <103964322+ViniciusMendez@users.noreply.github.com>
2022-05-20 17:45:08 -0300

    Condições Múltiplas JS i
    IF ELSE dentro de IF e ELSE e SWITCH

---
+++ aula12/aula013.js
@@ -0,0 +1,33 @@
+// CONDIÇÃO MULTIPLA  ESTRUTURA SWITCH A CADA CASE USAR O COMANDO BRAKE OBRIGATORIAMENTE
+
+var agora = new Date()
+var diaSem = agora.getDay()
+
+//console.log(diaSem)
+switch(diaSem){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda - Feira')
+        break  
+        
+    case 2:
+        console.log('Terça - Feira')
+        break   
+    case 3:
+        console.log('Quarta - Feira')
+        break      
+    case 4:
+        console.log('Quinta - Feira')
+        break    
+    case 5:
+        console.log('Sexta Feira')
+        break   
+    case 6:
+        console.log('Sabado')
+        break   
+    default:
+        console.log('erro dia Invalido')  
+        break    
+}

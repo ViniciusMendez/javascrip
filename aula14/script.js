b5c110695fd0013e597347584dda6b13be35020d
Vinicius Mendes <103964322+ViniciusMendez@users.noreply.github.com>
2022-05-25 19:16:46 -0300

    Update script.js

--- aula14/script.js
+++ aula14/script.js
@@ -9,7 +9,36 @@ function verificar(){
     } else{
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade Calculada: ${idade} anos`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+             genero = 'Homem'
+             if(idade >=0 && idade < 10){
+                 // CRIANÇA
+             } else if (idade< 21){
+                //JOVEM
+             } else if(idade <50){
+                 //ADULTO
+             } else{
+                 //IDOSO
+             }
+
+        } else if (fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >=0 && idade < 10){
+                // CRIANÇA
+            } else if (idade< 21){
+               //JOVEM
+            } else if(idade <50){
+                //ADULTO
+            } else{
+                //IDOSO
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos  ${genero} com ${idade} anos`
     }
 
 }
